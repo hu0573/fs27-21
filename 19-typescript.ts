30cd37860f1b5923082af10ea44b89e109c9915c
Ruiqing Hu <huruiqing0011@gmail.com>
2025-09-02 12:24:46 +0930

    init

---
+++ 19-typescript.ts
@@ -0,0 +1,9 @@
+//TypeScript 允许你给变量、函数参数和返回值指定类型，防止类型错误。
+//类型检查：TypeScript 在编译时进行类型检查，帮助捕获潜在的错误。而 JavaScript 是动态类型语言，运行时才会进行类型检查。
+
+//接口和类型系统：TypeScript 引入了接口（Interface）、类型别名（Type Alias）等类型系统特性，支持面向对象的编程方式。JavaScript 没有这些语法。
+
+//类的修饰符：TypeScript 支持访问控制修饰符（如 public、private、protected）来控制类成员的可访问性。JavaScript 中没有这些修饰符。
+
+
+
