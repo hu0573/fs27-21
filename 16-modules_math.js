5a150c49d746350ae701dbbd84c32877f6056a23
Ruiqing Hu <huruiqing0011@gmail.com>
2025-09-02 13:14:56 +0930

    import export modules

--- 16-modules_math.js
+++ 16-modules_math.js
@@ -1,3 +1,14 @@
 // math.js
 
 // 导出两个函数
+//export 用来导出函数、变量或对象，供其他文件使用。
+
+//import 用来从其他文件中导入导出的函数、变量或对象。
+
+export function add(a, b) {
+  return a + b;
+}
+
+export function subtract(a, b) {
+  return a - b;
+}
